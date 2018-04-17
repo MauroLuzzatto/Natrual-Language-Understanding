8a26cc3575a4d5e710dee49e8ae39bfc32629325
maurol <mauroluzzatto@hotmail.com>
2018-04-17 19:07:09 +0200

    update savin option

--- NLU_project1_tensorflow.py
+++ NLU_project1_tensorflow.py
@@ -4,8 +4,8 @@ NLU project 1
 
 import tensorflow as tf
 import numpy as np
-from load_embeddings import load_embedding
-from Preprocessing import preprocessing
+#from load_embeddings import load_embedding
+from Preprocessing9 import preprocessing
 
 class RNN_class(object):
     """ built the RNN """
@@ -16,9 +16,9 @@ class RNN_class(object):
         self.lstm_size = rnn_settings['lstm_size']
         self.vocabulary_size = rnn_settings['vocabulary_size']
         self.learning_rate = rnn_settings['learning_rate']
-        self.epoch_size = rnn_settings['epoch_size']
+        self.number_of_epochs = rnn_settings['number_of_epochs']
         self.clip_gradient = rnn_settings['clip_gradient']
-        self.num_batches = int(self.vocabulary_size/self.batch_size)
+        self.num_batches = int(self.vocabulary_size/self.batch_size) + 1
         self.training_mode = training_mode 
         
         # initialize the placeholders
@@ -32,7 +32,7 @@ class RNN_class(object):
             
         with tf.variable_scope('rnn_cell'):
             # Initial state of the LSTM memory.
-            lstm = tf.contrib.rnn.LSTMCell(self.lstm_size, forget_bias=0.0, state_is_tuple=True, reuse=not training_mode)
+            lstm = tf.contrib.rnn.LSTMCell(self.lstm_size, forget_bias=0.0, state_is_tuple=True)
         
         with tf.variable_scope('rnn_operations'):
             # rnn operation
@@ -46,7 +46,6 @@ class RNN_class(object):
                 (lstm_output, state) = lstm(embedded_inputs[:, word], state)
                 outputs.append(lstm_output)
             self.output = tf.reshape(tf.concat(outputs, 1), [-1, self.lstm_size])
-
 
         with tf.variable_scope('softmax_variables'):
             # Set model weights
@@ -66,16 +65,14 @@ class RNN_class(object):
         with tf.variable_scope('logits'):
             logits = tf.add(tf.matmul(self.output, W),b) 
             logits = tf.reshape(logits, [self.batch_size, self.sentence_length, self.vocabulary_size])
-        
+            self.sentence_probability = tf.nn.softmax(logits)
+            
         with tf.variable_scope('loss'):
-            # loss
             # TODO: reduce sum or mean? piazza says mean
             # negative or not?
-            
             self.loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits = logits, labels = self.input_y)
             self.loss =  tf.reduce_sum(self.loss, 1)
-        
-        if training_mode:
+            #tf.summary.scaler('loss', tf.reduce_sum(self.loss))
         
             with tf.variable_scope('predictions'):
                 self.predictions = tf.argmax(logits,2)  # [batch_size, sentence_length]
@@ -84,12 +81,14 @@ class RNN_class(object):
             
             with tf.variable_scope('accuracy'):
                 self.accuracy = tf.reduce_mean(tf.cast(correct, "float"), name="accuracy")
-            
-            with tf.variable_scope('training_operations'):
-                tvars = tf.trainable_variables()
-                grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), self.clip_gradient)
-                optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
-                self.train_op = optimizer.apply_gradients(zip(grads, tvars), global_step=tf.train.get_or_create_global_step())
+                tf.summary.scalar('accuracy', self.accuracy)
+                
+            if training_mode:
+                with tf.variable_scope('training_operations'):
+                    tvars = tf.trainable_variables()
+                    grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), self.clip_gradient)
+                    optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
+                    self.train_op = optimizer.apply_gradients(zip(grads, tvars), global_step=tf.train.get_or_create_global_step())
                 
             print('output', self.output.get_shape(), self.output.dtype)
             print('x size', self.input_x.get_shape(), self.input_x.dtype)
@@ -99,66 +98,111 @@ class RNN_class(object):
             print('pred size', self.predictions.get_shape(), self.predictions.dtype)
             #print (tf.trainable_variables())
             
-            self.perplexity = self.calculate_perplexity(logits = logits, 
-                                                        sentence_length = self.sentence_length,
-                                                        batch_size = self.batch_size)
-    
-#    def calculate_perplexity(self, y_true, y_pred):
-#        
-#        def log2(x):
-#          x = tf.cast(x, tf.float64)
-#          numerator = tf.log(x)
-#          denominator = tf.log(tf.constant(2, dtype=numerator.dtype))
-#          return tf.cast(numerator / denominator, tf.int64)
-#        
-#        cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_true * log2(y_pred), reduction_indices=[1]))
-#        perplexity = tf.pow(tf.cast(2, tf.int64), cross_entropy)
-#        return perplexity     
-    
-    def calculate_perplexity(self, logits, sentence_length, batch_size):
-        sentence_probability = tf.nn.softmax(logits)
+            # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
+            self.merged = tf.summary.merge_all()
         
-        def log2(x):
-          x = tf.cast(x, tf.float64)
-          numerator = tf.log(x)
-          denominator = tf.log(tf.constant(2, dtype=numerator.dtype))
-          return tf.cast(numerator / denominator, tf.int64)
-      
-        log_calc = log2(sentence_probability)
-        log_sum = tf.reduce_sum(log_calc,[1,2])/sentence_length
-        log_sum = tf.reshape(log_sum, [batch_size])
-        print('per', log_sum.get_shape())
-        return tf.pow(tf.cast(2, tf.float64), -log_sum)
+            
+    def create_submission_file(self, perplexity_list, task = 'A'):
+        # create the submission file
+        fileName = 'groupXX.perplexity' + task
+        with open(fileName, 'w') as file_handler:
+            for item in perplexity_list:
+                file_handler.write("{}\n".format(item))
+        print('output file created for task: ', task)
     
+    
+    def calculate_perplexity(self, sentence_probability, input_y, sentence_length):
+        probability = sentence_probability[:,:,input_y]
+        log_sum = np.sum(np.log2(probability),1)/sentence_length
+        return np.power(2, -log_sum)
+    
+    
+    def test_rnn(self, model, session, X, y, task):
+        """Runs the model on the test data"""
+                
+        pathToLog = r'./logTest/'
+        writer = tf.summary.FileWriter(pathToLog)
+        writer.add_graph(session.graph)
+        
+        perplexity_list = []
+        costs = 0.0
+        iters = 0.0
+        
+        num_test_batches = int(len(y)/self.batch_size)
+        
+        for batch_i in range(num_test_batches):
+            # get batches
+            start = batch_i * self.batch_size
+            end = min((batch_i + 1) * self.batch_size, len(y))
+            
+            feed_dict = {self.input_x: X[start:end],
+                         self.input_y: y[start:end]}
+                
+            loss, acc, pred, summary, \
+            sentence_probability = session.run([self.loss, self.accuracy, self.predictions, self.merged, self.sentence_probability], 
+                                               feed_dict)
+
+            # per_test = self.calculate_perplexity(sentence_probability, y[start:end], self.sentence_length)
+            #print ('test preplexity: ', np.mean(per_test))
+            
+            costs += loss
+            iters += self.sentence_length # we don't need this, since we are taking the mean
+            # TODO: check perplexity
+            perplexity = np.exp(costs/iters) 
+            print('Test set: loss: ', np.sum(loss), 'accuracy: ', acc, 'perplexity: ', np.mean(perplexity))
+            writer.add_summary(summary, batch_i)
+            perplexity_list.extend(perplexity)
+        
+        # create final suubmission file for a specific task
+        self.create_submission_file(perplexity_list = perplexity_list, task = task)
+        writer.add_summary(summary)
+        writer.flush()
+        writer.close()
+       
+        
     def train_rnn(self, model, session, X, y):
         """Runs the model on the given data."""
-
         costs = 0.0
-        iters = 0
-    
+        iters = 0.0
+        
+        pathToLog = r'./log/'
+        writer = tf.summary.FileWriter(pathToLog)
+        writer.add_graph(session.graph)
+                
         # iterate over all epochs
-        for epoch in range(self.epoch_size):
-             print ('epoch: ', epoch, self.num_batches)
+        for epoch_i in range(self.number_of_epochs):
+             ### shuffle?
+             print('#epoch: ', epoch_i) 
              # iterate over all batches
              for batch_i in range(self.num_batches):
                 # get batches
                 start = batch_i * self.batch_size
-                end = min((batch_i + 1) * self.batch_size, len(X))
+                end = min((batch_i + 1) * self.batch_size, len(y))
+                
+                feed_dict = {self.input_x: X[start:end],
+                             self.input_y: y[start:end]}
                     
-                _, loss, acc, pred, per_new = session.run([self.train_op, self.loss, self.accuracy, self.predictions, self.perplexity], 
-                                           feed_dict = {self.input_x: X[start:end],
-                                                        self.input_y: y[start:end]})
+                _, loss, acc, pred, summary, \
+                sentence_probability = session.run([self.train_op, self.loss, self.accuracy, self.predictions, self.merged, self.sentence_probability], 
+                                                   feed_dict)
+    
+                # per_test = self.calculate_perplexity(sentence_probability, y[start:end], self.sentence_length)
+                #print ('test preplexity: ', np.mean(per_test))
+                
                 costs += loss
                 iters += self.sentence_length # we don't need this, since we are taking the mean
                 # TODO: check perplexity
-                perplexity = np.exp(costs/iters) * np.log(2) # convert nats to bits
-                print('Training: batch: ', batch_i , 'loss: ', np.sum(loss), 'accuracy: ', acc, 'perplexity: ', np.sum(perplexity))
-               
-
-def main_train(train_X, train_Y, eval_X, eval_Y, word_dict):            
-    # reset the built graph
-    tf.reset_default_graph()
+                perplexity = np.exp(costs/iters) 
+                print('Training: batch: ', batch_i , 'loss: ', np.sum(loss), 'accuracy: ', acc, 'perplexity: ', np.mean(perplexity))
+                writer.add_summary(summary, epoch_i)
+                writer.flush()
         
+        writer.close()
+       
+
+
+def main_train(train_X, train_Y, eval_X, eval_Y, words_to_idx):            
+   
     rnn_settings = {
         'sentence_length' : 30-1,        
         'batch_size' : 64, # given
@@ -166,49 +210,80 @@ def main_train(train_X, train_Y, eval_X, eval_Y, word_dict):
         'lstm_size' : 512,
         'vocabulary_size' : 20000,
         'learning_rate' : 0.001, # default
-        'epoch_size' : 1,
-        'clip_gradient' : 5.0
+        'number_of_epochs' : 200,
+        'clip_gradient' : 5.0,
+        'Training_mode': False,
+        'Task': 'A'
         }
+
+    training_mode = rnn_settings['Training_mode']
+    task = rnn_settings['Task']
     
     rnn_train = RNN_class(rnn_settings, training_mode = True)
-    rnn_valid = RNN_class(rnn_settings, training_mode = False)
-
     # Launch the graph
     with tf.Session() as session:
-        # Initialize the variables 
-        session.run(tf.global_variables_initializer())
-        
-        
-        task_B = True
-        if task_B:   
-            embedding_matrix= tf.get_variable(name="embedding", initializer = tf.random_uniform([20000, 100], -0.1, 0.1))
-            load_embedding(session = session, 
-                           vocab = word_dict, 
-                           emb = embedding_matrix, 
-                           path = r'C:\Users\mauro\Desktop\CAS\_Natural Language Understanding\Project\data\wordembeddings-dim100.word2vec', 
-                           dim_embedding = 100, 
-                           vocab_size = 20000)
-        
 
-        # train the model
-        rnn_train.train_rnn(rnn_train, session, train_X, train_Y)
-        
-        # validate the model
-        rnn_valid.test_rnn(rnn_valid, session, eval_X, eval_Y)
-        # evaluate the model
-        #model.train(model, session, )
-
-        
-        
+        if training_mode:    
+            saver = tf.train.Saver()
+            # Initialize the variables 
+            session.run(tf.global_variables_initializer())
+            # train the model
+            rnn_train.train_rnn(rnn_train, session, train_X, train_Y)
+            # export the trained meta-graph
+            saver.save(session, 'graph/model.ckpt')
+        else:
+            # Launch session
+            #with tf.Session() as session:
+            ###session.run(tf.global_variables_initializer())
+            # load data from trained graph
             
+            saver = tf.train.Saver()
+            saver.restore(session, 'graph/model.ckpt')
+            print('model restored')
+#            new_saver = tf.train.import_meta_graph('graph/trained_graph.meta')
+#            new_saver.restore(session, tf.train.latest_checkpoint('graph'))
+        
+            # validate the model
+            rnn_train.test_rnn(rnn_train, session, eval_X, eval_Y, task)
+            
+      
+ # reset the built graph
+tf.reset_default_graph()
+
 # run the main method
-pathData = r'C:\Users\mauro\Desktop\CAS\_Natural Language Understanding\Project\data'
-train_file = pathData + '\sentences.train'
-test_file = pathData + '\sentences.eval'
-
-train_X, train_Y, word_dict = preprocessing(train_file)      
-#eval_X, eval_Y, word_dict = preprocessing(test_file)      
-
-main_train(train_X, train_Y, eval_X, eval_Y, word_dict)            
+#pathData = r'C:\Users\mauro\Desktop\CAS\_Natural_Language_Understanding\Project\data'
+#pathData=r"/home/dario/Desktop/NLU/Projekt/Data"
+trainFile = 'Data/sentences.train'
+testFile =  'Data/sentences.eval'
 
 
+train_X, train_Y, words_to_idx, word_dict = preprocessing(pathToFile = trainFile, 
+                                                          training_mode = True, 
+                                                          words_to_idx = None, 
+                                                          word_dict = None)
+  
+eval_X, eval_Y, words_to_idx, word_dict = preprocessing(pathToFile = testFile, 
+                                                        training_mode = False, 
+                                                        words_to_idx = words_to_idx, 
+                                                        word_dict = word_dict)     
+
+main_train(train_X, train_Y, eval_X, eval_Y, words_to_idx)            
+
+
+
+#if task == 'B':   
+#embedding_matrix= tf.get_variable(name="embedding", initializer = tf.random_uniform([20000, 100], -0.1, 0.1))
+#load_embedding(session = session, 
+#               vocab = words_to_idx, 
+#               emb = embedding_matrix, 
+#               path = r'C:\Users\mauro\Desktop\CAS\_Natural Language Understanding\Project\data\wordembeddings-dim100.word2vec', 
+#               dim_embedding = 100, 
+#               vocab_size = 20000)
+      
+
+
+
+
+#
+
+#        
--- Preprocessing.py
+++ Preprocessing.py
@@ -6,26 +6,21 @@ Created on Tue Apr 03 08:08:48 2018
 """
 
 import numpy as np
-#import tensorflow as tf
-from WordFrequency2 import WordFrequencyDist_D
+from WordFrequency2_m5 import WordFrequencyDist_D
 
 
-def preprocessing(filename):
-    
-    # load data
-    text_file_object = open(filename,'r')
-    raw_scentences = text_file_object.readlines()
-    text_file_object.close()
-    
-    words = []
-    for scent in raw_scentences:
-        word_scent = scent.split()
-        words.extend(word_scent)
-    
-    word_dist = WordFrequencyDist_D(words)
-    # <unk>, <pad>, <eos>, <bos> are not words, therfore only 19996
-    word_dict = {word_dist[i][0]: word_dist[i][1] for i in range(0, 19996)}
-    
+def preprocessing(pathToFile, training_mode, words_to_idx, word_dict):
+
+    def build_vocabulary(raw_scentences):
+        words = []
+        for scent in raw_scentences:
+            word_scent = scent.split()
+            words.extend(word_scent)
+        word_dist = WordFrequencyDist_D(words)
+        # <unk>, <pad>, <eos>, <bos> are not words, therfore only 19996
+        word_dict = {word_dist[i][0]: word_dist[i][1] for i in range(0, 19996)}
+        return word_dict
+        
     def Tokenize(raw_scentences, word_dict):
         """
         Takes raw scentences and the word dictinary as input and returns the 
@@ -37,6 +32,7 @@ def preprocessing(filename):
         """
         words_final = []
         tokenized = []
+        batchsize = 64
         for scent in raw_scentences:
             word_scent = scent.split()
             for i in range(0,len(word_scent)):
@@ -45,29 +41,50 @@ def preprocessing(filename):
                     
             sentence = ['<bos>']
             if len(word_scent) <= 28:
-    #            scentence.extend(word_scent[0:27])
-    #            scentence.extend(['<eos>'])
-    #        else:
                 sentence.extend(word_scent)
                 sentence.extend(['<eos>'])
                 sentence.extend(['<pad>']*(28-len(word_scent)))
                 
                 tokenized.append(sentence)
                 words_final.extend(sentence)
-        return tokenized, words_final
-                
-    
-    new_scentences, words_final = Tokenize(raw_scentences, word_dict)
-    word_dist_final = WordFrequencyDist_D(words_final)
-    word_dict_final = {word_dist_final[i][0]: word_dist_final[i][1] for i in range(0, 20000)}
-
-#    print('check:', len(word_dist_final), '/20000')
-    
-    words_to_idx = {word_dist_final[indx][0]: indx for indx in range(0, 20000)}
-    ## Create the training data
-    X = [[words_to_idx[w] for w in sent[:-1]] for sent in new_scentences]
-    y = [[words_to_idx[w] for w in sent[1:]] for sent in new_scentences]
         
-    return X,y, words_to_idx
+        nr_of_dummy_scentences = batchsize -  len(tokenized)%batchsize
+        print('len tok ', len(tokenized))
+        print('nr dummy scent ', nr_of_dummy_scentences)
+        for _ in range(0,nr_of_dummy_scentences):
+            tokenized.append(['<pad>']*30)
+                
+        return tokenized, words_final
+
+    # load data
+    text_file_object = open(pathToFile,'r')
+    raw_scentences = text_file_object.readlines()
+    text_file_object.close()
+             
+
+    if training_mode:
+        print('preprocessing for training data')
+        # build up the dictionary
+        word_dict = build_vocabulary(raw_scentences)   
+        
+        new_scentences, words_final = Tokenize(raw_scentences, word_dict)
+        
+        word_dist_final = WordFrequencyDist_D(words_final)
+        words_to_idx = {word_dist_final[indx][0]: indx for indx in range(0, 20000)}
+        ## Create the training data
+        X = [[words_to_idx[w] for w in sent[:-1]] for sent in new_scentences]
+        y = [[words_to_idx[w] for w in sent[1:]] for sent in new_scentences]
+        
+    
+    else:
+        print('preprocessing for test data')
+        # process the test data
+        new_scentences, words_final = Tokenize(raw_scentences, word_dict)
+        
+        X = [[words_to_idx[w] for w in sent[:-1]] for sent in new_scentences]
+        y = [[words_to_idx[w] for w in sent[1:]] for sent in new_scentences]
+    
+    return X,y, words_to_idx, word_dict
 
 
+    
